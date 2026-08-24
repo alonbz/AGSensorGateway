d610a205496a208f69b58fdbd23c2e8eae667e4e
alonbz <alonbz@gmail.com>
2026-08-24 09:59:36 +0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.agsense.ksensorgateway"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.agsense.ksensorgateway"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 5
+        versionName = "1.0.5"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
